e8343d974f402579382921945cc004bfb73e83c5
reethuo <152209084+reethuo@users.noreply.github.com>
2025-01-29 17:20:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "jenkins_vm" {
+  name         = "jenkins-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"  # Use Debian 11 image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}  # Enables external IP
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y nginx git
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+    git clone https://github.com/reethuo/gcpTfJenkins.git /var/www/html/
+  EOF
+
+  tags = ["web-server"]
+}
+
+output "instance_ip" {
+  value = google_compute_instance.jenkins_vm.network_interface[0].access_config[0].nat_ip
+}
